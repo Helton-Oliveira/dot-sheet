e20290b5028b65d77d44b39d198ac507f8b077b0
Helton-Oliveira <heltonhenriqueoliveira321@gmail.com>
2024-03-02 12:54:45 -0300

    refactor

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import "dotenv/config";
 import app from "./app.js";
 
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
   console.log(`servidor excutando na porta ${PORT}`);
