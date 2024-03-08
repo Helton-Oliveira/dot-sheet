bcfc5056a761e753c2c9cb7e8dfd325b94d85069
Helton-Oliveira <heltonhenriqueoliveira321@gmail.com>
2024-03-07 22:38:19 -0300

    feat: swagger documentation

--- routes/index.js
+++ routes/index.js
@@ -5,16 +5,18 @@ import registerRoutes from './registersRoutes.js';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import swaggerUi from 'swagger-ui-express';
+import fs from 'fs'
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const swaggerPath = path.join(__dirname, '../swagger.json');
+const swaggerDocs = JSON.parse(fs.readFileSync(swaggerPath))
 
 const routes = (app) => {
 
     app.use(express.static(path.join(__dirname, '../views'))) 
     app.use('/uploads', express.static(path.join(__dirname, '../uploads')))
-    app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerPath))
+    app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs))
     
     app.use((req, res, next) => {
         res.header('Access-Control-Allow-Origin', '*'); 
